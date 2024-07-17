9df8c90561b0dae151b7b7e1cb4de68dbc367836
k1mjunyoung <me@kimjunyoung.com>
2024-07-17 16:31:05 +0900

    MANITO-develop
    - 버그수정

--- src/main/java/manito/server/controller/ManitoServerController.java
+++ src/main/java/manito/server/controller/ManitoServerController.java
@@ -2,11 +2,13 @@ package manito.server.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/")
 public class ManitoServerController {
     @GetMapping
     public String main() {
-        return "/main";
+        return "main";
     }
 }
