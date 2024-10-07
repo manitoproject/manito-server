0dbcc2f723472d0e8017b823624017604dff3df5
k1mjunyoung <me@kimjunyoung.com>
2024-10-07 13:36:52 +0900

    MANITO-11 메세지 개수 조회 API 추가

---
+++ src/main/java/manito/server/dto/MessageCountDto.java
@@ -0,0 +1,13 @@
+package manito.server.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+public class MessageCountDto {
+    private Integer count;
+}
